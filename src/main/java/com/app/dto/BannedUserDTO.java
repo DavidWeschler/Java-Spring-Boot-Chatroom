97d5472c41c952de8135839101b217686ee29170
Ron Avital <ronavital2@gmail.com>
2025-07-13 13:13:39 +0300

    dto cleanup complete

--- src/main/java/com/app/dto/BannedUserDTO.java
+++ src/main/java/com/app/dto/BannedUserDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
@@ -12,9 +11,4 @@ public class BannedUserDTO {
     private Long id;
     private String username;
     private LocalDateTime bannedUntil;
-
-    public String getFormattedBannedUntil() {
-        if (bannedUntil == null) return "Forever";
-        return bannedUntil.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-    }
 }
